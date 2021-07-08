3f4e60885277110e4d8b6a90f91d3fbbf4b31eee
enginepdf <yeindap@gmail.com>
2021-07-08 23:32:01 +0900

    Create main.dart
    created Flutter folder and first_app project using Text widget. considering the size of the actual project, I didn't include the whole files.

---
+++ Flutter/first_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {  // main() => runApp()
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('First App'),
+      ),
+    ),
+  );
+}
